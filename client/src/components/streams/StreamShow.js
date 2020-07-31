dff33303eac4c4b8702a278929dbfc1c7f5ccee1
farahexa2020 <farahexa2020@gmail.com>
2020-07-31 18:04:29 +0300

    Add video streaming.

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,17 +1,53 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
   }
 
+  componentDidMount() {
+    this.streamId = this.props.match.params.id;
+
+    this.props.fetchStream(this.streamId);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.streamId}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  };
+
   render() {
+    if (!this.props.stream) {
+      return <div>Lodaing...</div>;
+    }
+
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <p>{description}</p>
       </div>
